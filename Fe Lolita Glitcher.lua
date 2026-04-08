621babaaef6d46dc890da3ebc0966e7fe79547cd
User14353 <86834923+User14353@users.noreply.github.com>
2026-04-08 15:32:15 +1000

    Update Fe Lolita Glitcher.lua

--- Fe Lolita Glitcher.lua
+++ Fe Lolita Glitcher.lua
@@ -138,12 +138,12 @@ local function RunReanimate()
 			
 			targets.rs = cf(
 			    Vector3.new(0.0000002, -0.0000006, 0.0000010),
-			    Vector3.new(-27.98, 47.61, 149.32)
+			    Vector3.new(-27.98, -47.61, 149.32)
 			)
 			
 			targets.lh = cf(
 			    Vector3.new(0, 0, 0),
-			    Vector3.new(-1.64, -23.58, -7.91)
+			    Vector3.new(-1.64, 23.58, -7.91)
 			)
 			
 			targets.rh = cf(
